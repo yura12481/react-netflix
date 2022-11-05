6b1e46f3e71153fe894f830a84157ae7dc6cb114
Yura Manchura <yura124812481@gmail.com>
2022-11-05 18:26:33 +0200

    add

--- src/components/Auth/SignIn/SignIn.tsx
+++ src/components/Auth/SignIn/SignIn.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './Footer/Footer'
 import Form from './Form/Form'
 import Header from './Header/Header'
 import style from './SignIn.module.css'
@@ -8,6 +9,7 @@ const SignIn = () => {
     <div className={style.authWrapper}>
       <Header/>
       <Form/>
+      <Footer/>
     </div>
   )
 }
